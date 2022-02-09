0343619e9a39f24cfe0e9a71c8631c3ff1806d8e
Michael <MichaelCheung101@gmail.com>
2022-02-08 22:00:10 -0500

    commit tree ef1b1e3ca0ed21e3f56d19b12ecca36981f5b136

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,25 @@
-import React from "react";
-import NewPlantForm from "./NewPlantForm";
-import PlantList from "./PlantList";
-import Search from "./Search";
+import React, { useEffect, useState } from 'react';
+
+import NewPlantForm from './NewPlantForm';
+import PlantList from './PlantList';
+import Search from './Search';
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+      .then((response) => response.json())
+      .then((plantsArray) => {
+        setPlants(plantsArray);
+      });
+  }, []);
+
   return (
     <main>
       <NewPlantForm />
       <Search />
-      <PlantList />
+      <PlantList plants={plants} />
     </main>
   );
 }
